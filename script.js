b9cd3c5aa4890e4f9bbe9b98d971c287d7856f95
guillaume-ruer <guillaume.ruer@dpi.fr>
2021-11-24 10:18:00 +0100

    touch slider

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// nav
 const hamburger = document.getElementById('hamburger');
 const navUl = document.getElementById('nav-ul');
 
@@ -6,98 +7,89 @@ hamburger.addEventListener('click', () => {
 });
 
 // slider
-if (navigator.msMaxTouchPoints) {
+const slider = document.querySelector('.slider-container');
+const slides = Array.from(document.querySelectorAll('.slide'));
 
-    $('#slider').addClass('ms-touch');
+let isDragging = false;
+let startPos = 0;
+let currentTranslate = 0;
+let prevTranslate = 0;
+let animationID = 0;
+let currentIndex = 0;
+
+// Disable context menu
+window.oncontextmenu = function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
+}
+
+slides.forEach((slide, index) => {
+  const slideImage = slide.querySelector('img');
+  slideImage.addEventListener('dragstart', (e) => e.preventDefault());
+
+  // Touch events
+  slide.addEventListener('touchstart', touchStart(index));
+  slide.addEventListener('touchend', touchEnd);
+  slide.addEventListener('touchmove', touchMove);
   
-    $('#slider').on('scroll', function() {
-      $('.slide-image').css('transform','translate3d(-' + (100-$(this).scrollLeft()/6) + 'px,0,0)');
-    });
-  
-  } else {
-  
-    var slider = {
-  
-      el: {
-        slider: $("#slider"),
-        holder: $(".holder"),
-        imgSlide: $(".slide-image")
-      },
-  
-      slideWidth: $('#slider').width(),
-      touchstartx: undefined,
-      touchmovex: undefined,
-      movex: undefined,
-      index: 0,
-      longTouch: undefined,
-      
-      init: function() {
-        this.bindUIEvents();
-      },
-  
-      bindUIEvents: function() {
-  
-        this.el.holder.on("touchstart", function(event) {
-          slider.start(event);
-        });
-  
-        this.el.holder.on("touchmove", function(event) {
-          slider.move(event);
-        });
-  
-        this.el.holder.on("touchend", function(event) {
-          slider.end(event);
-        });
-  
-      },
-  
-      start: function(event) {
-        // Test for flick.
-        this.longTouch = false;
-        setTimeout(function() {
-          window.slider.longTouch = true;
-        }, 250);
-  
-        // Get the original touch position.
-        this.touchstartx =  event.originalEvent.touches[0].pageX;
-  
-        // The movement gets all janky if there's a transition on the elements.
-        $('.animate').removeClass('animate');
-      },
-  
-      move: function(event) {
-        // Continuously return touch position.
-        this.touchmovex =  event.originalEvent.touches[0].pageX;
-        // Calculate distance to translate holder.
-        this.movex = this.index*this.slideWidth + (this.touchstartx - this.touchmovex);
-        // Defines the speed the images should move at.
-        var panx = 100-this.movex/6;
-        if (this.movex < 600) { // Makes the holder stop moving when there is no more content.
-          this.el.holder.css('transform','translate3d(-' + this.movex + 'px,0,0)');
-        }
-        if (panx < 100) { // Corrects an edge-case problem where the background image moves without the container moving.
-          this.el.imgSlide.css('transform','translate3d(-' + panx + 'px,0,0)');
-        }
-      },
-  
-      end: function(event) {
-        // Calculate the distance swiped.
-        var absMove = Math.abs(this.index*this.slideWidth - this.movex);
-        // Calculate the index. All other calculations are based on the index.
-        if (absMove > this.slideWidth/2 || this.longTouch === false) {
-          if (this.movex > this.index*this.slideWidth && this.index < 2) {
-            this.index++;
-          } else if (this.movex < this.index*this.slideWidth && this.index > 0) {
-            this.index--;
-          }
-        }      
-        // Move and animate the elements.
-        this.el.holder.addClass('animate').css('transform', 'translate3d(-' + this.index*this.slideWidth + 'px,0,0)');
-        this.el.imgSlide.addClass('animate').css('transform', 'translate3d(-' + 100-this.index*50 + 'px,0,0)');
-  
-      }
-  
-    };
-  
-    slider.init();
+  // Mouse events
+  slide.addEventListener('mousedown', touchStart(index));
+  slide.addEventListener('mouseup', touchEnd);
+  slide.addEventListener('mouseleave', touchEnd);
+  slide.addEventListener('mousemoce', touchMove);
+})
+
+function touchStart(index) {
+  return function(event) {
+    currentIndex = index;
+    startPos = getPositionX(event);
+    isDragging = true;
+
+    animationID = requestAnimationFrame(animation);
+    slider.classList.add('grabbing');
   }
+}
+
+function touchEnd() {
+  isDragging = false;
+  cancelAnimationFrame(animationID);
+
+  const movedBy = currentTranslate - prevTranslate;
+  if (movedBy < -100 && currentIndex < slides.length - 1) {
+    currentIndex += 1;
+  }
+  if (movedBy > 100 && currentIndex > 0) {
+    currentIndex -= 1;
+  }
+
+  setPositionByIndex();
+
+  slider.classList.remove('grabbing');
+}
+
+function touchMove(event) {
+  if (isDragging) {
+    const currentPosition = getPositionX(event);
+    currentTranslate = prevTranslate + currentPosition - startPos;
+  }
+}
+
+function getPositionX(event) {
+  return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
+}
+
+function animation() {
+  setSliderPosition();
+  if (isDragging) requestAnimationFrame(animation);
+}
+
+function setSliderPosition() {
+  slider.style.transform = `translateX(${currentTranslate}px)`;
+}
+
+function setPositionByIndex() {
+  currentTranslate = currentIndex * -window.innerWidth;
+  prevTranslate = currentTranslate;
+  setSliderPosition();
+}
